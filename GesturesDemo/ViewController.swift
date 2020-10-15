881264a7c0b8c7a7fceb02a1599d7440c6e2324d
aleneweaver11 <aleneweaver11@gmail.com>
2020-10-15 10:30:38 -0400

    mvp

--- GesturesDemo/ViewController.swift
+++ GesturesDemo/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var myimageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func imageviewTapped(_ sender: UITapGestureRecognizer)
+    {
+        print("Demogoron tapped")
+        view.backgroundColor = UIColor.black
+    }
+    
+    @IBAction func starPanned(_ sender: UIPanGestureRecognizer)
+    {
+         let translation = sender.translation(in: view)
+        guard let gestureView = sender.view else {
+            return
+        }
+        gestureView.center = CGPoint (
+            x: gestureView.center.x + translation.x,
+            y: gestureView.center.y + translation.y )
+        sender.setTranslation(.zero, in: view)
+        }
+        
+    }
 
-}
 
